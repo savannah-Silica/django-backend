40048aaafb698c40502ee6ff6f28aa8e9e186788
madbunny <madbunny@madbunny.madbunny>
2022-10-21 20:18:52 +0300

    Load-Secret-Key-as-ENV-variable

--- community/settings.py
+++ community/settings.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,7 +11,7 @@ SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*h1g1j2=4xjf1$dfw0wa+#uq9n18xz*_o8@w93b$4wqy8ry$=4'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
