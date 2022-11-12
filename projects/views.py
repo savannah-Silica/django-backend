1bbdddd4e3e75379e566dd60bcec89c0347a0b2d
Kibet-mutai <kibetem24@gmail.com>
2022-11-13 02:14:30 +0300

    Added Projects ListView

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from .models import Project
+from.serializers import ProjectSerializer
+from rest_framework import status
+from rest_framework.response import Response
 
 # Create your views here.
+@api_view(['GET', 'POST'])
+def ListView(request):
+
+    """
+    List all projects, or create a new project.
+    """
+    
+    if request.method == 'GET':
+        projects = Project.objects.all()
+        serializer = ProjectSerializer(projects, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = ProjectSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
