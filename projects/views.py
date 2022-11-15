96d2d5d38f7d3141ec1cb4b3f86c6ec4ca91383f
Kibet Mutai <109361613+Kibet-mutai@users.noreply.github.com>
2022-11-15 22:01:08 +0300

    project Detailview: #34 (#38)
    * Added Projects ListView
    
    * added project detailview
    
    * Projects detailview urls
    
    Co-authored-by: Omariba Collins <88287473+Collins-Omariba@users.noreply.github.com>

--- projects/views.py
+++ projects/views.py
@@ -6,6 +6,35 @@ from rest_framework import status
 from rest_framework.response import Response
 
 # Create your views here.
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def DetailView(request, id):
+
+    """
+    Retrieve, update or delete a project.
+    """
+
+    try:
+        projects = Project.objects.all(id=id)
+    except Project.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = ProjectSerializer(projects)
+        return Response(serializer.data)
+
+    elif  request.method == 'PUT':
+        serializer = ProjectSerializer(projects, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    elif request.method == 'DELETE':
+        projects.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 @api_view(['GET', 'POST'])
 def ListView(request):
 
@@ -24,3 +53,4 @@ def ListView(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
