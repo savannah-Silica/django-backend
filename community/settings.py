6a5c750cf5f9def09dfc9e5d0853a19f300fdfe0
IanoNjuguna <ichege438@gmail.com>
2022-11-18 18:50:31 +0300

    community: Add database configuration

--- community/settings.py
+++ community/settings.py
@@ -105,8 +105,12 @@ ASGI_APPLICATION = 'community.asgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
