ac0b1d4d4ad55388109195daac33436d229a9e3f
Omariba Collins <88287473+Collins-Omariba@users.noreply.github.com>
2022-11-23 12:16:54 +0300

    Removed repeated code in team model.

--- projects/models.py
+++ projects/models.py
@@ -48,8 +48,8 @@ class Category(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField(max_length=255)
 
-    def __str__(self):
-        return f'{self.title}'
+
+
 
     class Meta:
         verbose_name_plural = 'Categories'
