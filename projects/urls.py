3ffd90573f9bf2eb8b68694f1c8de322836c8787
Barissa-Imran <imranbarissa@gmail.com>
2023-01-21 15:32:12 +0300

    Fix error in detail view
    Fixes: 68

--- projects/urls.py
+++ projects/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
 
-    path('project_detail/<int:id>/', views.DetailView),
+    path('project_detail/<int:pk>/', views.ProjectDetail.as_view()),
     path('projects_list/', views.ListView),
 ]
 
--- projects/views.py
+++ projects/views.py
@@ -2,38 +2,42 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from .models import Project
 from.serializers import ProjectSerializer
-from rest_framework import status
+from rest_framework import status, generics
 from rest_framework.response import Response
+
 
 # Create your views here.
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def DetailView(request, id):
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def DetailView(request, id):
 
-    """
-    Retrieve, update or delete a project.
-    """
+#     """
+#     Retrieve, update or delete a project.
+#     """
 
-    try:
-        projects = Project.objects.all(id=id)
-    except Project.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+#     try:
+#         projects = Project.objects.all(id=id)
+#     except Project.DoesNotExist:
+#         return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'GET':
-        serializer = ProjectSerializer(projects)
-        return Response(serializer.data)
+#     if request.method == 'GET':
+#         serializer = ProjectSerializer(projects)
+#         return Response(serializer.data)
 
-    elif  request.method == 'PUT':
-        serializer = ProjectSerializer(projects, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     elif  request.method == 'PUT':
+#         serializer = ProjectSerializer(projects, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-    elif request.method == 'DELETE':
-        projects.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+#     elif request.method == 'DELETE':
+#         projects.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+class ProjectDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
 
 @api_view(['GET', 'POST'])
 def ListView(request):
@@ -41,7 +45,7 @@ def ListView(request):
     """
     List all projects, or create a new project.
     """
-    
+
     if request.method == 'GET':
         projects = Project.objects.all()
         serializer = ProjectSerializer(projects, many=True)
