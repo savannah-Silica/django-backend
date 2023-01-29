4b4cd76e4a3119e2910bff8b2c15f2f81aff2075
Kibet-mutai <kibetem24@gmail.com>
2023-01-29 18:39:47 +0300

    Setup Pagination

--- community/settings.py
+++ community/settings.py
@@ -157,3 +157,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination'
+}
